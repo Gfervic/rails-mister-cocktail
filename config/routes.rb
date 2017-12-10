6c1e31fa69b79159beee990bcc9d1d128e0928db
gon.fervic <gon.fervic@gmail.com>
2017-12-10 18:23:33 +0000

    implemented cokctail & doses controller, cocktail show with new dose form and new routing for the ingredient show page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   resources :cocktails, only: [:index, :show, :new, :create] do
     resources :doses, only: [:create, :destroy]
+
+  resources :ingredients, only [:show]
   end
 end
 
