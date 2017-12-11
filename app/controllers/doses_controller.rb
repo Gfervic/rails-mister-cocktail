f9f6f9c82e18527f613670bc0d67170449b6f402
gon.fervic <gon.fervic@gmail.com>
2017-12-11 01:45:15 +0000

    add cocktail#destroy functionality, a dose can be deleted.. went back to pass rake test with doses#new in doses controller and with its own new.html.erb view

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,6 +1,10 @@
 class DosesController < ApplicationController
 
-  before_action :set_cocktail
+  before_action :set_cocktail, except: [:destroy]
+
+  def new
+    @dose = Dose.new
+  end
 
   def create
     @dose = Dose.new(dose_params)
@@ -9,12 +13,13 @@ class DosesController < ApplicationController
     if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
-      render "cocktails/show"
+      render :new
     end
   end
 
   def destroy
     @dose = Dose.find (params[:id])
+    @cocktail = @dose.cocktail
     @dose.destroy
     redirect_to cocktail_path(@cocktail)
   end
