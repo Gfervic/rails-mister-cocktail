01325108a84c0084da296782260d04ceb20a9377
gon.fervic <gon.fervic@gmail.com>
2017-12-20 11:56:26 +0100

    review model ok, implement partial form and review#new

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,8 +2,6 @@ class Review < ApplicationRecord
   belongs_to :cocktail
 
   validates :content, :rating, presence: true
-
   validates :rating, inclusion: {in: (0..10).to_a}, numericality: { only_integer: true }
   validates :cocktail_id, presence: true
-
 end
